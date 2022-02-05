f329a23f0d858bc030099e26912ce0234d61be9b
Lorand98 <kalmarlorand@yahoo.com>
2022-02-05 17:04:08 +0200

    Article, ArticleDetails, ArticlePhotos components created

---
+++ src/components/Article/Article.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+import classes from './Artcile.module.scss';
+
+export default class Article extends Component {
+  render() {
+    return <article></article>;
+  }
+}
---
+++ src/components/Article/ArticleDetails.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+import classes from './ArtcileDetails.module.scss';
+
+export default class ArticleDetails extends Component {
+  render() {
+    return <div></div>;
+  }
+}
