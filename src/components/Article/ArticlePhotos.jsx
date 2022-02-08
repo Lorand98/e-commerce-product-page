02a65e6d89a98702f7ce324fcb97195d3f0c6599
Lorand98 <kalmarlorand@yahoo.com>
2022-02-08 17:18:09 +0200

    Selecting a thumbnail makes the large photo switch to the large version of that thumbnail (desktop mode)

--- src/components/Article/ArticlePhotos.jsx
+++ src/components/Article/ArticlePhotos.jsx
@@ -2,55 +2,61 @@ import React, { Component } from 'react';
 
 import classes from './ArticlePhotos.module.scss';
 
-import thumbnail1 from '../../images/products/image-product-1-thumbnail.jpg';
-import thumbnail2 from '../../images/products/image-product-2-thumbnail.jpg';
-import thumbnail3 from '../../images/products/image-product-3-thumbnail.jpg';
-import thumbnail4 from '../../images/products/image-product-4-thumbnail.jpg';
-import photo1 from '../../images/products/image-product-1.jpg';
-import photo2 from '../../images/products/image-product-2.jpg';
-import photo3 from '../../images/products/image-product-3.jpg';
-import photo4 from '../../images/products/image-product-4.jpg';
+import thumbnailPath1 from '../../images/products/image-product-1-thumbnail.jpg';
+import thumbnailPath2 from '../../images/products/image-product-2-thumbnail.jpg';
+import thumbnailPath3 from '../../images/products/image-product-3-thumbnail.jpg';
+import thumbnailPath4 from '../../images/products/image-product-4-thumbnail.jpg';
+import photoPath1 from '../../images/products/image-product-1.jpg';
+import photoPath2 from '../../images/products/image-product-2.jpg';
+import photoPath3 from '../../images/products/image-product-3.jpg';
+import photoPath4 from '../../images/products/image-product-4.jpg';
 
 export default class ArticlePhotos extends Component {
+  articlePhotoPaths = [
+    { photoPath: photoPath1, thumbnailPath: thumbnailPath1 },
+    { photoPath: photoPath2, thumbnailPath: thumbnailPath2 },
+    { photoPath: photoPath3, thumbnailPath: thumbnailPath3 },
+    { photoPath: photoPath4, thumbnailPath: thumbnailPath4 },
+  ];
+
+  constructor() {
+    super();
+    this.state = {
+      selectedPhoto: this.articlePhotoPaths[0],
+    };
+  }
+
+  selectPhotoHandler(selectedPhotoIndex) {
+    this.setState({
+      selectedPhoto: this.articlePhotoPaths[selectedPhotoIndex],
+    });
+  }
+
   render() {
     return (
       <div className={classes['article-photos']}>
         <img
           alt='Shoe'
-          src={photo1}
+          src={this.state.selectedPhoto.photoPath}
           className={classes['article-photos__main-photo']}
         />
         <div className={classes['article-photos__thumbnail-box']}>
-          <div
-            className={`${classes['article-photos__thumbnail-container']} ${classes['article-photos__thumbnail-container--selected']}`}
-          >
-            <img
-              alt='Shoe'
-              src={thumbnail1}
-              className={classes['article-photos__thumbnail']}
-            />
-          </div>
-          <div className={classes['article-photos__thumbnail-container']}>
-            <img
-              alt='Shoe'
-              src={thumbnail2}
-              className={classes['article-photos__thumbnail']}
-            />
-          </div>
-          <div className={classes['article-photos__thumbnail-container']}>
-            <img
-              alt='Shoe'
-              src={thumbnail3}
-              className={classes['article-photos__thumbnail']}
-            />
-          </div>
-          <div className={classes['article-photos__thumbnail-container']}>
-            <img
-              alt='Shoe'
-              src={thumbnail4}
-              className={classes['article-photos__thumbnail']}
-            />
-          </div>
+          {this.articlePhotoPaths.map(({ thumbnailPath }, currElIndex) => (
+            <div
+              className={`${classes['article-photos__thumbnail-container']}  ${
+                thumbnailPath === this.state.selectedPhoto.thumbnailPath &&
+                classes['article-photos__thumbnail-container--selected']
+              }`}
+              key={currElIndex}
+              onClick={this.selectPhotoHandler.bind(this, currElIndex)}
+            >
+              <img
+                alt='Shoe'
+                src={thumbnailPath}
+                className={classes['article-photos__thumbnail']}
+              />
+            </div>
+          ))}
         </div>
       </div>
     );
