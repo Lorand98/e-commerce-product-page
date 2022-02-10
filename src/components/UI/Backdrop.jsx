ead5c4f39dce7ced27633412170634ead2644a4c
Lorand98 <kalmarlorand@yahoo.com>
2022-02-10 18:07:11 +0200

    More responsivity issues fixed: -photo height set to max 70vh on phone, so it doesnt fill up the whole space in case the height is too small -overflow removed from the main photo container -Modal made responsive for small height screen

--- src/components/UI/Backdrop.jsx
+++ src/components/UI/Backdrop.jsx
@@ -4,7 +4,6 @@ import classes from './Backdrop.module.scss';
 
 export default class Backdrop extends Component {
   render() {
-    console.log([classes['backdrop'], this.props.className].join(' '));
     return (
       <div
         className={[classes['backdrop'], this.props.className].join(' ')}
